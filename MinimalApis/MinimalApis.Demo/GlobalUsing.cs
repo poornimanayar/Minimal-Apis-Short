4fa72f7c3c883af9780395ef032dd4246f59c989
Poornima Nayar <poornimakrishnav@gmail.com>
2022-11-24 08:08:43 +0000

    demo project

---
+++ MinimalApis/MinimalApis.Demo/GlobalUsing.cs
@@ -0,0 +1,4 @@
+﻿global using Microsoft.AspNetCore.Http.HttpResults;
+global using Microsoft.AspNetCore.OutputCaching;
+global using Microsoft.AspNetCore.RateLimiting;
+global using System.Threading.RateLimiting;
---
+++ MinimalApis/MinimalApis.Demo/Person.cs
@@ -0,0 +1,9 @@
+﻿public class Person
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public bool HasAPetUnicorn { get; set; }
+
+}
